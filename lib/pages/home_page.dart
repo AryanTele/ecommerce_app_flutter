2c273be379b1ac1731d4421228cd987af77e5f03
Aryan Tele <telearyan12@gmail.com>
2024-06-20 20:27:11 +0530

    add Shop sectiom

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:ecommerce_app/components/bottom_nav_bar.dart';
+import 'package:ecommerce_app/pages/cart_page.dart';
+import 'package:ecommerce_app/pages/shop_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -20,13 +22,94 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  // pages for navigation
+  final List<Widget> _pages = [
+    //shop page
+    const ShopPage(),
+
+    // cart page
+    const CartPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 228, 227, 227),
+      backgroundColor: const Color.fromARGB(255, 228, 227, 227),
       bottomNavigationBar: MyBottomNavBar(
         onTabChange: (index) => navigateBottomBar(index),
       ),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Builder(
+            builder: (context) => IconButton(
+                  icon: const Padding(
+                    padding: EdgeInsets.only(left: 12.0),
+                    child: Icon(
+                      Icons.menu,
+                      color: Colors.black,
+                    ),
+                  ),
+                  onPressed: () {
+                    Scaffold.of(context).openDrawer();
+                  },
+                )),
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.black,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                //logo
+                DrawerHeader(
+                  child: Image.asset(
+                    'lib/images/nike-logo.png',
+                    color: Colors.white,
+                  ),
+                ),
+                // Padding(
+                //   padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                //   child: Divider(
+                //     color: Colors.grey[800],
+                //   ),
+                // ),
+
+                //links
+                const Padding(
+                  padding: EdgeInsets.only(top: 25.0, left: 25.0),
+                  child: ListTile(
+                    leading: Icon(Icons.home),
+                    title: Text('Home'),
+                    iconColor: Colors.white,
+                    textColor: Colors.white,
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 25.0),
+                  child: ListTile(
+                    leading: Icon(Icons.info),
+                    title: Text('About'),
+                    iconColor: Colors.white,
+                    textColor: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 25.0, bottom: 25.0),
+              child: ListTile(
+                leading: Icon(Icons.logout),
+                title: Text('Logout'),
+                iconColor: Colors.white,
+                textColor: Colors.white,
+              ),
+            )
+          ],
+        ),
+      ),
+      body: _pages[_selectedIndex],
     );
   }
 }
